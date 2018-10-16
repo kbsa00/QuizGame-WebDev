d2e7b9343474450b8fedb3b9f0e738fe652c7a6d
Khalid B.S <khalid_bs@hotmail.com>
2018-10-16 10:22:15 +0200

    heroku

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-    "mongo_URI": "mongodb://adminkhalid:adminkhalid123@ds123753.mlab.com:23753/pgrexam_prod",
-    "cookie_key": "lsjdøsakdaslødasøldsadasdsa",
-    "google_clientID": "345798135609-ebqvrmfl98isv6l45cq8116tkma9aje6.apps.googleusercontent.com",
-    "google_clientsecretID": "GOo6fCYJtErlipqtqM_HbmOw"
+    "mongo_URI": process.env.MONGO_URI,
+    "cookie_key": process.env.COOKIE_KEY,
+    "google_clientID": process.env.GOOGLE_CLIENTID,
+    "google_clientsecretID": process.env.GOOGLE_CLIENTSECRETID
 }
