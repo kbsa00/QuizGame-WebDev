2d20cce11ff8aad9b8a9346cbc2c432649af694e
Khalid B.S <khalid_bs@hotmail.com>
2018-10-16 19:04:24 +0200

    Still fixing react part

--- server.js
+++ server.js
@@ -7,13 +7,16 @@ require('./services/passport');
 require('./middleware/appMiddlewares')(app); 
 require('./routes/authRoutes')(app);
 
-if(process.env.NODE == 'production'){
-    app.use(express.static(path.join(__dirname, 'client/dist')));
-    const path = require('path')
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/dist'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname+'/client/dist/index.html'));
+      res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
     });
-}
+  }
 
 const PORT = process.env.PORT || 3000; 
 
