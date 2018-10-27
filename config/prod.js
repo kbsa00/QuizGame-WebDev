fdc97954fd94a18024d580e51fc806e6d27cd00a
Khalid B.S <khalid_bs@hotmail.com>
2018-10-27 16:06:46 +0200

    Heroku push

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,5 @@
 module.exports = {
     "mongo_URI": process.env.MONGO_URI,
-    "JWT_KEY": process.env.JWT_KEY
+    "JWT_KEY": process.env.JWT_KEY,
+    "COOKIE_SECRET_KEY": process.env.COOKIE_SECRET_KEY
 }
