f210ecb3300f57fba8d6fee38147b326a4b0ba0a
Khalid B.S <khalid_bs@hotmail.com>
2018-11-16 12:58:48 +0100

    heroku fix

--- middleware/appMiddlewares.js
+++ middleware/appMiddlewares.js
@@ -19,20 +19,20 @@ module.exports = (app) => {
         saveUninitialized: false
     }));
     
-    app.use(bodyParser.json());
-    app.use(passport.initialize());
-    app.use(passport.session());
-    app.use(cors());
-    
-    mongoose.set('useCreateIndex', true);
-    mongoose.connect(keys.mongo_URI, {
-        useNewUrlParser: true
-    });
-    
+
     if (process.env.NODE_ENV === 'production') {
         app.use(express.static('client/dist'));
         app.get('*', (req, res) => {
             res.sendFile(path.resolve(__dirname, '..','client', 'dist', 'index.html'));
         });
     }
+
+    app.use(bodyParser.json());
+    app.use(passport.initialize());
+    app.use(passport.session());
+    
+    mongoose.set('useCreateIndex', true);
+    mongoose.connect(keys.mongo_URI, {
+        useNewUrlParser: true
+    });
 }
